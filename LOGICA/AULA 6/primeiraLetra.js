dd88e82c855cb85d114a145f3418821832b06591
KarineSilvaa <karineereiis0@gmail.com>
2023-01-24 21:51:16 -0300

    atualização

--- LOGICA/AULA 6/primeiraLetra.js
+++ LOGICA/AULA 6/primeiraLetra.js
@@ -3,6 +3,7 @@ console.log("== Setor de RH ==");
 function primeiraLetra(nome) {
     console.log("o conteúdo da var nome é", nome);
     var letra = nome.substring(0, 1);
+    letra = letra.toLowerCase();
     console.log("A primeira letra é",letra);
 }
 
