6dd3bc1cfcc37c4204a96aaf78e73ec37ffc1165
KarineSilvaa <karineereiis0@gmail.com>
2023-02-06 19:43:52 -0300

    Atualização Aula 9

--- LOGICA/AULA 9/chamada.js
+++ LOGICA/AULA 9/chamada.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")();
 
-const alunos = ["Karine","Thais", "Alex","Maxwell","Chris","Giancarlo","Carlos"];
+const alunos = ["Karine","Thais", "Alex","Maxwell","Chris","Giancarlo","Carlos","Alexandre","Aline"];
 
 for(var i = 0; i < alunos.length; i++);{
     const nome = alunos[1];
