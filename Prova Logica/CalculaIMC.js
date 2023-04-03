e107b32265a9736c85d8aa9b13c8e8724763b830
KarineSilvaa <karineereiis0@gmail.com>
2023-04-03 12:06:57 -0300

    Atualização Prova Logica

--- Prova Logica/CalculaIMC.js
+++ Prova Logica/CalculaIMC.js
@@ -1,61 +1,40 @@
-// Professor não consegui fazer esse codigo funcionar
+// Define a classe Paciente
+class Paciente {
+  constructor(nome, peso, altura) {
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+  }
 
-pacientes = [
-    {'nome': 'Luke Skywalker', 'peso': 68, 'altura': 1.72},
-    {'nome': 'Darth Vader', 'peso': 136, 'altura': 2.02},
-    {'nome': 'Leia Organa', 'peso': 55, 'altura': 1.55},
-    {'nome': 'Han Solo', 'peso': 85, 'altura': 1.8},
-    {'nome': 'Mestre Yoda', 'peso': 30, 'altura': 0.66},
-    {'nome': 'Chewbacca', 'peso': 200, 'altura': 2.28},
-    {'nome': 'C-3PO', 'peso': 75, 'altura': 1.67},
-    {'nome': 'R2-D2', 'peso': 32, 'altura': 0.96}
-]
+  // Calcula o IMC do paciente
+  calcularIMC() {
+    return this.peso / (this.altura * this.altura);
+  }
 
-categorias = {
-    'baixo peso': {'min': 0, 'max': 18.4},
-    'normal': {'min': 18.5, 'max': 24.9},
-    'sobrepeso': {'min': 25, 'max': 29.9},
-    'obesidade': {'min': 30, 'max': float('inf')}
+  // Retorna a categoria do IMC do paciente
+  categoriaIMC() {
+    const imc = this.calcularIMC();
+    if (imc < 18.5) {
+      return "baixo peso";
+    } else if (imc < 25) {
+      return "normal";
+    } else if (imc < 30) {
+      return "sobrepeso";
+    } else {
+      return "obesidade";
+    }
+  }
 }
 
-for (paciente in pacientes)
-    imc = paciente['peso'] / (paciente['altura'] ** 2)
-    paciente['imc'] = imc
-    for (categoria, limites in categorias.items())
-    if (limites['min'] <= imc <= limites['max']);
-    paciente['categoria'] = categoria
-    break;
+// Cria os objetos Paciente
+const pacientes = [
+  new Paciente("Luke Skywalker", 80, 1.8),
+  new Paciente("Darth Vader", 150, 1.85),
+  new Paciente("Leia Organa", 50, 1.6),
+  // adicione mais pacientes aqui
+];
 
-
-for (categoria in categorias)
-    pacientes_categoria = [ for  in pacientes ]
-      if ( 'categoria' == categoria)
-    if (categoria == 'sobrepeso')
-        print('Sobrepeso:')
-        for (paciente in pacientes_categoria)
-            print('- ' + paciente['nome'] + ', IMC: ' + str(paciente['imc']))
-    else categoria == 'obesidade';
-        print('Obesidade:')
-        for (paciente in pacientes_categoria)
-            print('- ' + paciente['nome'] + ', IMC: ' + str(paciente['imc']))
-    else
-        print(categoria.capitalize() + ':')
-        for (paciente in pacientes_categoria)
-            print('- ' + paciente['nome'] + ', IMC: ' + str(paciente['imc']))
-    
-total_por_categoria = {}
-for (categoria in categorias)
-    pacientes_categoria = [p for p in pacientes if p['categoria'] == categoria]
-    total_por_categoria[categoria] = len(pacientes_categoria)
-print('\nTotal de pacientes por categoria:')
-for (categoria, total in total_por_categoria.items())
-    print('- ' + categoria.capitalize() + ': ' + str(total))
-    
-
-total_sobrepeso_obesidade = 0
-for (paciente in pacientes)
-    if (paciente['categoria'] == 'sobrepeso') or paciente['categoria'] == 'obesidade'
-        total_sobrepeso_obesidade += 1
-print('\nTotal de pacientes com sobrepeso ou obesidade: ' + str(total_sobrepeso_obesidade))
-
-
+// Mostra a categoria do IMC de cada paciente
+for (const paciente of pacientes) {
+  console.log(`${paciente.nome} = ${paciente.categoriaIMC()}`);
+}
--- Prova Logica/ListaPacientes.js
+++ Prova Logica/ListaPacientes.js
@@ -1,48 +1,73 @@
-// Professor não consegui fazer esse codigo funcionar
+class Paciente {
+  constructor(nome, idade, peso, altura) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+  }
 
-class Paciente
-    {def} __init__(self, nome, peso, altura);
-        self.nome = nome
-        self.peso = peso
-        self.altura = altura
-    
-    {def} calcular_imc(self);
-        return self.peso / (self.altura * self.altura)
-    
-    {def} __str__(self);
-        return self.nome
+  calcularIMC() {
+    let imc = this.peso / (this.altura ** 2);
+    return imc.toFixed(2);
+  }
 
-{def} relatorio_por_categoria(pacientes, categoria);
-    pacientes_categoria = []
+  verificarCategoria() {
+    let imc = this.calcularIMC();
+    if (imc < 18.5) {
+      return "Abaixo do peso";
+    } else if (imc >= 18.5 && imc < 25) {
+      return "Peso normal";
+    } else if (imc >= 25 && imc < 30) {
+      return "Sobrepeso";
+    } else {
+      return "Obesidade";
+    }
+  }
+}
 
-    for (paciente in pacientes);
-        imc = paciente.calcular_imc() 
+let pacientes = [
+  new Paciente("Darth Vader", 45, 92, 1.87),
+  new Paciente("Mestre Yoda", 900, 70, 0.66),
+  new Paciente("Luke Skywalker", 25, 75, 1.8),
+  new Paciente("Princesa Leia", 28, 60, 1.6),
+  new Paciente("Han Solo", 35, 95, 1.85),
+];
 
-        if (categoria == 'baixo peso' and imc < 18.5):
-            pacientes_categoria.append(paciente)
-        elif categoria == 'normal' and imc >= 18.5 and imc <= 24.9:
-            pacientes_categoria.append(paciente)
-        elif categoria == 'sobrepeso' and imc >= 25 and imc <= 29.9:
-            pacientes_categoria.append(paciente)
-        elif categoria == 'obesidade' and imc >= 30:
-            pacientes_categoria.append(paciente)
-    if categoria == 'sobrepeso':
-        print('Sobrepeso:')
-        for paciente in pacientes_categoria:
-            print(paciente)
-    elif categoria == 'obesidade':
-        print('Obesidade:')
-        for paciente in pacientes_categoria:
-            print(paciente)
-    else:
-        print(categoria.capitalize() + ':')
-        for paciente in pacientes_categoria:
-            print(paciente)
+// Relatório por categoria
+let categorias = {
+  "Abaixo do peso": [],
+  "Peso normal": [],
+  Sobrepeso: [],
+  Obesidade: [],
+};
 
-def total_pacientes_por_categoria(pacientes):
-    categorias = ['baixo peso', 'normal', 'sobrepeso', 'obesidade']
-    total_por_categoria = {}
-    for categoria in categorias:
-        total_por_categoria[categoria] = 0
-    for paciente in pacientes:
-       
+for (let paciente of pacientes) {
+  let categoria = paciente.verificarCategoria();
+  categorias[categoria].push(paciente);
+}
+
+console.log("Relatório por categoria:");
+for (let categoria in categorias) {
+  console.log(`${categoria}:`);
+  for (let paciente of categorias[categoria]) {
+    console.log(` - ${paciente.nome} (${paciente.idade} anos) - IMC: ${paciente.calcularIMC()}`);
+  }
+}
+
+// Total de pacientes por categoria
+console.log("\nTotal de pacientes por categoria:");
+for (let categoria in categorias) {
+  console.log(`${categoria}: ${categorias[categoria].length}`);
+}
+
+// Quantidade de pacientes com sobrepeso ou obesidade
+let pacientesSobrepesoObesidade = categorias.Sobrepeso.concat(categorias.Obesidade);
+console.log(`\nTotal de pacientes com sobrepeso ou obesidade: ${pacientesSobrepesoObesidade.length}`);
+console.log("Sobrepeso:");
+for (let paciente of categorias.Sobrepeso) {
+  console.log(` - ${paciente.nome}`);
+}
+console.log("Obesidade:");
+for (let paciente of categorias.Obesidade) {
+  console.log(` - ${paciente.nome}`);
+}
